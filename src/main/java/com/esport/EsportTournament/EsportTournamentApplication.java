ab015d15fb96d3210b8e8e6bc3be8f6646ca8632
time <aryan3340s@gmail.com>
2025-11-27 21:48:37 +0530

    removed the deploy error

--- src/main/java/com/esport/EsportTournament/EsportTournamentApplication.java
+++ src/main/java/com/esport/EsportTournament/EsportTournamentApplication.java
@@ -3,9 +3,6 @@ package com.esport.EsportTournament;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-@SpringBootApplication
-@EnableScheduling
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
